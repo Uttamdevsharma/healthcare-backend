c09240935c7e4106195497df3bae913700631861
Uttam <uttamrohit4545@gmail.com>
2026-04-10 21:17:40 +0600

    feat : additional fields in auth.ts

---
+++ prisma/migrations/20260410151542_additionals/migration.sql
@@ -0,0 +1,14 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('SUPER_ADMIN', 'ADMIN', 'DOCTOR', 'PATIENT');
+
+-- CreateEnum
+CREATE TYPE "UserStatus" AS ENUM ('BLOCKED', 'DELETED', 'ACTIVE');
+
+-- CreateEnum
+CREATE TYPE "Gender" AS ENUM ('MALE', 'FEMALE');
+
+-- AlterTable
+ALTER TABLE "user" ADD COLUMN     "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN     "needPasswordChange" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN     "role" "Role" NOT NULL DEFAULT 'PATIENT',
+ADD COLUMN     "status" "UserStatus" NOT NULL DEFAULT 'ACTIVE';
