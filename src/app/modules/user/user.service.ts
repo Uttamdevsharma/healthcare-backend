6e9481fda9c5ecce292a285826320c0946836e0a
Uttam <uttamrohit4545@gmail.com>
2026-04-22 11:49:02 +0600

    feat : doctor create

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -5,6 +5,7 @@ import { auth } from "../../lib/auth";
 import { prisma } from "../../lib/prisma";
 import { ICreateDoctorPayload } from "./user.interface";
 
+
 const createDoctor = async (payload: ICreateDoctorPayload) => {
 
     const specialties: Specialty[] = [];
