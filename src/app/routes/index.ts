8ab9dcc9f20ad23c15227bbb6667adc7649c85cb
Uttam <uttamrohit4545@gmail.com>
2026-04-22 11:59:00 +0600

    feat : doctore create controller and route setup

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,11 +2,13 @@
 import {Router} from 'express'
 import { AuthRoutes } from '../modules/auth/auth.routes'
 import { SpecialtyRoutes } from '../modules/speciality/speciality.routes'
+import { UserRouter } from '../modules/user/user.route'
 
 const router = Router()
 
 
 router.use("/auth",AuthRoutes)
 router.use("/specialties",SpecialtyRoutes)
+router.use("/users",UserRouter)
 
 export const IndexRoutes = router
