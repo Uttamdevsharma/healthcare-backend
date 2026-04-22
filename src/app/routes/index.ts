df95f2c21575e84c4f5ebb0ad4341d39d1532ce4
Uttam <uttamrohit4545@gmail.com>
2026-04-22 10:22:06 +0600

    feat : specialty module

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,10 +1,12 @@
 
 import {Router} from 'express'
 import { AuthRoutes } from '../modules/auth/auth.routes'
+import { SpecialtyRoutes } from '../modules/speciality/speciality.routes'
 
 const router = Router()
 
 
 router.use("/auth",AuthRoutes)
+router.use("/specialties",SpecialtyRoutes)
 
 export const IndexRoutes = router
