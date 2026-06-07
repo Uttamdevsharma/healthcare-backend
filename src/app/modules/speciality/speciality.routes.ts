4f10ac2553e6a10c4b25d5449770bbd90568c94e
Uttam <uttamrohit4545@gmail.com>
2026-06-07 23:33:30 +0600

    feat : multer single file set

--- src/app/modules/speciality/speciality.routes.ts
+++ src/app/modules/speciality/speciality.routes.ts
@@ -1,3 +1,4 @@
+import { multerUpload } from './../../config/multer.config';
 import { checkAuth } from './../../middleware/checkAuth';
 import { Router } from "express";
 import { SpecialtyController } from "./speciality.controller";
@@ -5,7 +6,7 @@ import { Role } from '../../../generated/prisma/enums';
 const router = Router();
 
 
-router.post("/", checkAuth(Role.SUPER_ADMIN,Role.ADMIN),SpecialtyController.createSpecialty);
+router.post("/", checkAuth(Role.SUPER_ADMIN,Role.ADMIN),multerUpload.single("file"), SpecialtyController.createSpecialty);
 router.get("/", SpecialtyController.getAllSpecialties)
 router.delete("/:id",checkAuth(Role.ADMIN,Role.SUPER_ADMIN),SpecialtyController.deleteSpecialty)
 
