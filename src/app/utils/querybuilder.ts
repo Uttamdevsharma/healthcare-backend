52b2280d42f3aa011cc45519435fb719229ecd59
Uttam <uttamrohit4545@gmail.com>
2026-06-09 22:45:30 +0600

    feat : search query builder

--- src/app/utils/querybuilder.ts
+++ src/app/utils/querybuilder.ts
@@ -34,4 +34,70 @@ TInclude = Record<string, unknown>
             where : {},
         }
     }
+
+    search() : this {
+        const {searchTerm} = this.queryParams;
+        const { searchableFields} = this.config;
+        // doctorSearchableFields = ['user.name', 'user.email', 'specialties.specialty.title' , 'specialties.specialty.description']
+        if(searchTerm && searchableFields && searchableFields.length > 0){
+            const searchConditions : Record<string, unknown>[] = searchableFields.map((field) => {
+                if(field.includes(".")){
+                    const parts = field.split(".");
+
+                    if(parts.length === 2){
+                        const [relation, nestedField] = parts;
+
+                        const stringFilter : PrismaStringFilter = {
+                            contains : searchTerm,
+                            mode : 'insensitive' as const,
+                        }
+
+                        return {
+                            [relation] : {
+                                [nestedField] : stringFilter
+                            }
+                        }
+                    }else if(parts.length === 3){
+                        const [relation, nestedRelation, nestedField] = parts;
+
+                        const stringFilter : PrismaStringFilter = {
+                            contains : searchTerm,
+                            mode : 'insensitive' as const,
+                        }
+
+                        return {
+                            [relation] : {
+                                some :{
+                                    [nestedRelation]: {
+                                        [nestedField]: stringFilter
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    
+                }
+                // direct field
+                const stringFilter: PrismaStringFilter = {
+                    contains: searchTerm,
+                    mode: 'insensitive' as const,
+                }
+
+                return {
+                    [field]: stringFilter
+                }
+            }
+        )
+
+        const whereConditions = this.query.where as PrismaWhereConditions
+
+        whereConditions.OR = searchConditions;
+
+        const countWhereConditions = this.countQuery.where as PrismaWhereConditions;
+        countWhereConditions.OR = searchConditions;
+        }
+
+        return this;
+    }
 }
+
