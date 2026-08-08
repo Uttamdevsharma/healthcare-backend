1af3b7c8b76ea52e370f623418b2c9806bcf7064
Uttam <uttamrohit4545@gmail.com>
2026-08-08 17:30:36 +0600

    feat : redis service connect in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import app from "./app";
 import { envVars } from "./app/config/env";
+import { redisService } from "./app/lib/redis";
 import { seedSuperAdmin } from "./app/utils/seed";
 
 const bootstrap = async() => {
     try {
         await seedSuperAdmin();
+        await redisService.connect().catch(console.error);
         app.listen(envVars.PORT, () => {
             console.log(`Server is running on http://localhost:${envVars.PORT}`);
         });
